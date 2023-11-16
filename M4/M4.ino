ba938408b8453fbe450385dd4ed87167cf755c95
Angelo Giannuzzi <a.giannuzzi96@git.it>
2023-11-16 18:37:11 +0100

    Add code for keyboard handling

--- M4/M4.ino
+++ M4/M4.ino
@@ -1,10 +1,15 @@
 #include <RPC.h>
+#include "USBHostGiga.h"
 
 #define PinRESET 4
 #define ClockSigOUT 5
 
+Keyboard keyb;
+
 void setup() {
   RPC.begin();
+  pinMode(PA_15, OUTPUT);
+  keyb.begin();
   pinMode(ClockSigOUT, OUTPUT);
   pinMode(PinRESET, OUTPUT);
   reset();
@@ -16,6 +21,37 @@ void reset() {
   digitalWrite(PinRESET, LOW);
   delay(1000);
   digitalWrite(PinRESET, HIGH);
+}
+
+char keyboard_handler() {
+
+  if (keyb.available()) {
+
+    auto _key = keyb.read();
+    
+    _key.lshift = _key.lgui ? 1 : _key.lshift;
+    _key.lgui = _key.lgui ? 0 : _key.lgui;
+
+    _key.lctrl = _key.lalt ? 1 : _key.lctrl;
+    _key.lalt = _key.lalt ? 0 : _key.lalt;
+
+    _key.rctrl = _key.ralt ? 1 : _key.rctrl;
+    _key.ralt = _key.ralt ? 0 : _key.ralt;
+
+    _key.keys[0] = (_key.keys[0] == 0x2d) ? 0x2e : _key.keys[0];
+    _key.keys[0] = (_key.keys[0] == 0x2f) ? 0x2a : _key.keys[0];
+    _key.keys[0] = (_key.keys[0] == 0x34) ? 0x28 : _key.keys[0];
+    _key.keys[0] = (_key.keys[0] == 0x33) ? 0x31 : _key.keys[0];
+    _key.keys[0] = (_key.keys[0] == 0x65) ? 0x35 : _key.keys[0];
+
+    return keyb.getAscii(_key);
+
+  } else {
+
+    return 0;
+    
+  }
+
 }
 
 void loop() {
