f5c5d54fa78053c7a4554839fb3244d77ee98317
Angelo Giannuzzi <angelogiannuzzi96@outlook.it>
2024-11-12 19:29:28 +0100

    Code improvement

--- utilities/6502Parser.sh
+++ utilities/6502Parser.sh
@@ -7,33 +7,50 @@ if ! command -v jq > /dev/null; then
     exit 1
 fi
 
-filePath="libraries/rommanager/rommanager"
-fileName="UnixCore-6502.json"
+libraryFilePath="libraries/rommanager/rommanager"
+jsonFileName="UnixCore-6502.json"
 maxSIZE=16384
-modifiedFile=$(git diff --name-only ../${fileName})
+modifiedFile=$(git diff --name-only ../${jsonFileName})
+
+if ! jq empty ../$jsonFileName 2> /dev/null; then
+
+    echo "Error: The JSON file $jsonFileName is invalid. Please check the structure and try again!" >&2
+    exit 1
+
+fi
 
 if [ -z "$modifiedFile" ]; then
 
-    echo "The $fileName file has not been modified!" >&2
+    echo "The $jsonFileName file has not been modified!" >&2
     exit 1
 
 else
 
-    SIZE=$(jq -r '.code | join(" ")' ../$fileName | wc -w)
-    
-    if [ $SIZE -le $maxSIZE ]; then
-        
-        arrayElements=$(jq -r '.code | join(" ") | split(" ") | join(", ") | "{" + . + "}"' ../$fileName)
-        sed -i "s/byte lowrom\[16384\] = {[^}]*};/byte lowrom[16384] = ${arrayElements};/" ../${filePath}.cpp
-        sed -i "s/#define SIZE\s\+[0-9]\+/#define SIZE ${SIZE}/" ../${filePath}.h
-        echo "Done."
-        exit 0
-    
-    else
+    if invalidEntries=$(jq -r '.code[]' ../$jsonFileName | grep -Pv '^(0x[0-9a-fA-F]{2}( 0x[0-9a-fA-F]{2}){0,16})$'); then
 
-        echo "You have exceeded the allowed size!" >&2
+        echo "Invalid lines detected in ${jsonFileName}. These lines need to be reviewed and fixed:" >&2
+        echo "$invalidEntries" >&2
         exit 1
 
+    else
+
+        SIZE=$(jq -r '.code | join(" ")' ../$jsonFileName | wc -w)
+    
+        if [ $SIZE -le $maxSIZE ]; then
+        
+            arrayElements=$(jq -r '.code | join(" ") | split(" ") | join(", ") | "{" + . + "}"' ../$jsonFileName)
+            sed -i "s/byte lowrom\[16384\] = {[^}]*};/byte lowrom[16384] = ${arrayElements};/" ../${libraryFilePath}.cpp
+            sed -i "s/#define SIZE\s\+[0-9]\+/#define SIZE ${SIZE}/" ../${libraryFilePath}.h
+            echo "Done."
+            exit 0
+    
+        else
+
+            echo "You have exceeded the allowed size!" >&2
+            exit 1
+
+        fi
+    
     fi
 
 fi 
